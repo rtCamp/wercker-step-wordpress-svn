5bb6cffbc11a92065602a931825e10ed74b59625
Rahul Bansal <rahul.bansal@rtcamp.com>
2015-03-19 21:20:37 +0530

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 # Based on Dean Clatworthy's deploy script - https://github.com/deanc/wordpress-plugin-git-svn
 
+# Preparing
+apt-get install -y subversion
+
 # git source code
 # this file should be in the base of your git repository
 GITPATH="$WERCKER_SOURCE_DIR/"
